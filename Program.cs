6323fbaf1c0898bf17a37f516e410ca8c8291266
Patchrik <pklly92@gmail.com>
2020-12-01 10:49:28 -0600

    We got done with using the Interfaces to get this thing to do

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace garyGarage
 {
@@ -20,31 +21,95 @@ namespace garyGarage
     {
       Console.WriteLine("The Vehicle comes to a stop!");
     }
-
-    public double BatteryKWh { get; set; }
-    public void ChargeBattery()
-    {
-      Console.WriteLine("*ZAP* Fully Juiced BB!");
-    }
-
-    public double FuelCapacity { get; set; }
-    public void RefuelTank()
-    {
-      Console.WriteLine("Fill it up Eddy!");
-    }
-
   }
 
-  public class Zero : Vehicle // Electric motorcycle
+  // This is our interface for vehicles using ICEs
+  public interface IRunsOnGas
   {
+    double CurrentTankPercentage { get; set; }
+    double FuelCapacity { get; set; }
+    void RefuelTank();
+  }
+
+  // This is our interface for vehicles that are using electric engines bb
+  public interface IRunOnElectric
+  {
+    double BatteryKWh { get; set; }
+    double CurrentChargePercentage { get; set; }
+    void ChargeBattery();
+  }
+
+
+  public class Zero : Vehicle, IRunOnElectric // Electric motorcycle
+  {
+    public double BatteryKWh { get; set; } = 100;
+    public double CurrentChargePercentage { get; set; } = 0;
+    public void ChargeBattery()
+    {
+      this.CurrentChargePercentage = 100.00;
+      Console.WriteLine("*ZAP* Fully Juiced BB!");
+    }
     public override void Drive()
     {
       Console.WriteLine($"The {this.MainColor} Zero zips by you! Wahhhhhhhhhhh!");
     }
   }
-
-  public class Cessna : Vehicle // Propellor light aircraft
+  public class Scooter : Vehicle, IRunOnElectric // A scootin scooter
   {
+    public double BatteryKWh { get; set; } = 0;
+    public double CurrentChargePercentage { get; set; } = 0;
+
+    public void ChargeBattery()
+    {
+      this.CurrentChargePercentage = 100.00;
+      Console.WriteLine("*ZAP* Fully Juiced BB!");
+    }
+    public override void Drive()
+    {
+      Console.WriteLine($"The {this.MainColor} Scooter scoots by you.");
+    }
+  }
+
+  public class Jetski : Vehicle, IRunsOnGas // Water motorcycle
+  {
+    public double FuelCapacity { get; set; }
+    public double CurrentTankPercentage { get; set; } = 0;
+
+    public void RefuelTank()
+    {
+      this.CurrentTankPercentage = 100.00;
+      Console.WriteLine("Fill it up Eddy!");
+    }
+    public override void Drive()
+    {
+      Console.WriteLine($"The {this.MainColor} Jetski splashes by you! Waaaaasssshhhhhhhh!");
+    }
+  }
+
+  public class Boat : Vehicle, IRunsOnGas // Water motorcycle
+  {
+    public double FuelCapacity { get; set; }
+    public double CurrentTankPercentage { get; set; } = 0;
+    public void RefuelTank()
+    {
+      this.CurrentTankPercentage = 100.00;
+      Console.WriteLine("Fill it up Eddy!");
+    }
+    public override void Drive()
+    {
+      Console.WriteLine($"The {this.MainColor} Boat bobs passed you! DING DING!");
+    }
+  }
+
+  public class Cessna : Vehicle, IRunsOnGas // Propellor light aircraft
+  {
+    public double FuelCapacity { get; set; }
+    public double CurrentTankPercentage { get; set; } = 0;
+    public void RefuelTank()
+    {
+      this.CurrentTankPercentage = 100.00;
+      Console.WriteLine("Fill it up Eddy!");
+    }
 
     public override void Drive()
     {
@@ -56,8 +121,16 @@ namespace garyGarage
     }
   }
 
-  public class Tesla : Vehicle  // Electric car
+  public class Tesla : Vehicle, IRunOnElectric  // Electric car
   {
+    public double BatteryKWh { get; set; } = 0;
+    public double CurrentChargePercentage { get; set; } = 0;
+
+    public void ChargeBattery()
+    {
+      this.CurrentChargePercentage = 100.00;
+      Console.WriteLine("*ZAP* Fully Juiced BB!");
+    }
     public override void Drive()
     {
       Console.WriteLine($"The {this.MainColor} Tesla blazes by you! Vrrrrrmmmmmm!");
@@ -68,8 +141,15 @@ namespace garyGarage
     }
   }
 
-  public class Ram : Vehicle  // Gas powered truck
+  public class Ram : Vehicle, IRunsOnGas  // Gas powered truck
   {
+    public double FuelCapacity { get; set; }
+    public double CurrentTankPercentage { get; set; } = 0;
+    public void RefuelTank()
+    {
+      this.CurrentTankPercentage = 100.00;
+      Console.WriteLine("Fill it up Eddy!");
+    }
 
     public override void Drive()
     {
@@ -82,35 +162,82 @@ namespace garyGarage
     }
   }
 
+  public class RV : Vehicle, IRunsOnGas  // Gas powered truck
+  {
+    public double FuelCapacity { get; set; }
+    public double CurrentTankPercentage { get; set; } = 0;
+    public void RefuelTank()
+    {
+      this.CurrentTankPercentage = 100.00;
+      Console.WriteLine("Fill it up Eddy!");
+    }
+
+    public override void Drive()
+    {
+      Console.WriteLine($"The {this.MainColor} RV rumbles passed you!");
+    }
+
+    public override void Stop()
+    {
+      Console.WriteLine($"The {this.MainColor} RV comes rolling to a stop. WHERE ARE MY GRAND BABIES!");
+    }
+  }
+
   // Here is the Main Method for running our code.
   class Program
   {
     static void Main(string[] args)
     {
       Zero fxs = new Zero();
-      fxs.MainColor = "Dress Black";
-      fxs.MaximumOccupancy = 2;
+      Zero fx = new Zero();
       Tesla modelS = new Tesla();
-      modelS.MainColor = "Royal Blue";
-      modelS.MaximumOccupancy = 5;
-      Cessna mx420 = new Cessna();
-      mx420.MainColor = "Safety Yellow";
-      mx420.MaximumOccupancy = 3;
-      Ram r1500 = new Ram();
-      r1500.MainColor = "Biggest Peter Purple";
-      r1500.MaximumOccupancy = 6;
 
-      fxs.Drive();
-      modelS.Drive();
-      modelS.Turn("Left");
-      modelS.Stop();
-      mx420.Drive();
-      mx420.Turn("Right");
-      mx420.Stop();
-      r1500.Drive();
-      r1500.Turn("Left");
-      r1500.Stop();
+      List<IRunOnElectric> electricVehicles = new List<IRunOnElectric>() {
+        fx, fxs, modelS
+            };
 
+      Console.WriteLine("Electric Vehicles");
+      foreach (IRunOnElectric ev in electricVehicles)
+      {
+        Console.WriteLine($"{ev.CurrentChargePercentage}");
+      }
+
+      foreach (IRunOnElectric ev in electricVehicles)
+      {
+        // This should charge the vehicle to 100%
+        ev.ChargeBattery();
+      }
+
+      foreach (IRunOnElectric ev in electricVehicles)
+      {
+        Console.WriteLine($"{ev.CurrentChargePercentage}");
+      }
+
+      /***********************************************/
+
+      Ram ram = new Ram();
+      Cessna cessna150 = new Cessna();
+
+      List<IRunsOnGas> gasVehicles = new List<IRunsOnGas>() {
+        ram, cessna150
+            };
+
+      Console.WriteLine("Gas Vehicles");
+      foreach (IRunsOnGas gv in gasVehicles)
+      {
+        Console.WriteLine($"{gv.CurrentTankPercentage}");
+      }
+
+      foreach (IRunsOnGas gv in gasVehicles)
+      {
+        // This should completely refuel the gas tank
+        gv.RefuelTank();
+      }
+
+      foreach (IRunsOnGas gv in gasVehicles)
+      {
+        Console.WriteLine($"{gv.CurrentTankPercentage}");
+      }
     }
   }
 }
